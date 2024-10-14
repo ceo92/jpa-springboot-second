d89c45dcd8ee7d282cbe8d4e1b7dcdf06398c7e2
ceo92 <king9243@naver.com>
2024-10-14 15:27:43 +0900

    feat: 응답값 dto 및 제네릭을 통한 리스트 응답 JSON 방지 조회 생성 완료

--- src/main/java/jpa/mvc/api/OrderSimpleApiController.java
+++ src/main/java/jpa/mvc/api/OrderSimpleApiController.java
@@ -1,10 +1,13 @@
 package jpa.mvc.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import jpa.mvc.Address;
 import jpa.mvc.domain.Order;
+import jpa.mvc.domain.OrderStatus;
 import jpa.mvc.repository.OrderSearch;
 import jpa.mvc.service.OrderService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
@@ -19,13 +22,52 @@ public class OrderSimpleApiController {
   private final OrderService orderService;
 
   @GetMapping("/api/v1/simple-orders")
-  public List<Order> findOrders(){
+  public List<Order> findOrdersV1(){
     List<Order> orders = orderService.findOrders(new OrderSearch());
     for (Order order : orders) {
       order.getMember().getName();
       order.getDelivery().getAddress();
     }
     return orders;
+  }
+
+  //원래는 컬렉션 응답하면 확장에 어려우니 넘기면 안 되지만 예제이므로 ㅎㅎ
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDto> findOrdersV2(){
+    List<SimpleOrderDto> newList = orderService.findOrders(new OrderSearch()).stream().map(SimpleOrderDto::new).toList();
+    return newList;
+  }
+
+
+  @GetMapping("/api/v3/simple-orders")
+  public Result findOrdersV3(){
+    List<SimpleOrderDto> newList = orderService.findOrders(new OrderSearch()).stream().map(SimpleOrderDto::new).toList();
+    return new Result<>(newList); //이렇게 제네릭을 통해 일반적인 중괄호로 묶여진 JSON형태가 되게 해야됨 그래야 확장 가능 ㅇㅇ 추가적인 속성이 생겨도 !
+  }
+
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T>{
+    private T data;
+  }
+
+  @Data
+  static class SimpleOrderDto{
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public SimpleOrderDto(Order o){
+      orderId = o.getId();
+      name = o.getMember().getName();
+      orderDate = o.getOrderDate();
+      orderStatus = o.getOrderStatus();
+      address = o.getMember().getAddress();
+    }
+
   }
 
 }
